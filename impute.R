7840a4156654aa9e439524f904056833229173cb
matteobolner <matteo.bolner2@unibo.it>
2024-09-17 18:36:20 +0200

    Finished setting up CLI

--- impute.R
+++ impute.R
@@ -1,65 +1,108 @@
+#!/usr/bin/Rscript
+
 #install.packages("dplyr")
 #install.packages("tidyr")
 #install.packages("tibble")
-#install.packages("mice")
+#install.packages("readr")
+#install.packages("tools")
 #install.packages("readxl")
 #install.packages("optparse")
+#install.packages("mice")
 
-library(dplyr)
+
+#LOAD THE REQUIRED LIBRARIES
+library(mice,warn.conflicts = FALSE)
+library(readxl)
+library(dplyr,warn.conflicts = FALSE)
 library(tidyr)
 library(tibble)
 library(readr)
 library(tools)
-library(readxl)
 library(optparse)
-library(mice)
-
-#setwd("work/workspace/pigphenomics_metabolomics/")
-data_path="data/raw_data/data_filtered.tsv"
-chemical_annotation_path="config/metabolites.tsv"
 
 
-mice_random_seed=1000
-mice_imputation_method='pmm'
-metadata_predictors='sex, weight'
-n_imputations=5
-
-
-
+####################################################################################
+###PARSE COMMAND-LINE ARGUMENTS
+####################################################################################
 option_list <- list(
-  make_option(c("-d", "--data"), action="store",
-              help="Path to the file containing the metabolomics data"),
-  make_option(c("-c", "--chemical_annotation"), action="store",
-              help="Path to the file containing the metabolite chemical annotation"),
-  make_option(c("-s", "--seed"), type="integer", default=42,
-              help="Random seed for MICE imputation",
-              metavar="integer number"),
-  make_option(c("-m", "--method"), type="string", default="pmm",
-              help="Imputation method for MICE the default and recommended for most use-cases is pmm",
-              metavar="pmm | rf | logreg | polyreg | polr"),
-  make_option(c("-n", "--number_imputations"), type="string", default=5,
-              help="Number of imputed datasets to generate",
-              metavar="pmm | rf | logreg | polyreg | polr"),
-    make_option(c("-ap", "--additional-predictors"), type="string", default="aaa",
-              help="Comma-separated list of additional non-metabolite columns of the dataset to use as predictors, such as sex,weight etc",
-              metavar="sex,weght")
+  make_option(c("-d", "--data"), action = "store",
+              help = "Input file containing the metabolomics data in {tsv,csv,xlsx,xls} format",
+  ),
+  make_option(
+    c("-c", "--chemical_annotation"),
+    action = "store",
+    help = "Input file containing the metabolite chemical annotation in {tsv,csv,xlsx,xls} format"
+  ),
+  make_option(c("-o", "--output"), action = "store",
+              help = "Path to the file where the imputed datasets will be saved, in .tsv format"),
+  make_option(
+    c("-t", "--imputed_only"),
+    action = "store",
+    default = NULL,
+    help = "For testing purposes only: path to the file where only the imputed values will be saved, in .tsv format"
+  ),
+  make_option(
+    c("-s", "--seed"),
+    type = "integer",
+    default = 42,
+    help = "Random seed for MICE imputation",
+    metavar = "integer number"
+  ),
+  make_option(
+    c("-m", "--method"),
+    type = "character",
+    default = "pmm",
+    help = "Imputation method for MICE; the default and recommended for most use-cases is 'pmm', predictive mean matching. Other examples are rf | logreg | polyreg | polr",
+  ),
+  make_option(
+    c("-n", "--number_imputations"),
+    type = "integer",
+    default = 5,
+    help = "Number of imputed datasets to generate",
+  ),
+  make_option(
+    c("-a", "--additional_predictors"),
+    type = "character",
+    default = NULL,
+    help = "Comma-separated list of additional non-metabolite columns of the dataset to use as predictors, such as sex,weight etc",
+    metavar = "col1,col2"
+  )
   
 )
 
-opt_parser <- OptionParser(option_list=option_list)
+opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
-print(opt)
+
+if (is.null(opt$data)) {
+  stop("Input file path is mandatory. Use -d or --data to specify it.")
+}
+if (is.null(opt$chemical_annotation)) {
+  stop("Input file path is mandatory. Use -c or --chemical-annotion to specify it.")
+}
+if (is.null(opt$output)) {
+  stop("Output file path is mandatory. Use -o or --output to specify it.")
+}
 
 
+#Define arguments to use in the code
 
-n_imputations=as.numeric(n_imputations)
-mice_random_seed=as.numeric(mice_random_seed)
+n_imputations = opt$number_imputations
+mice_random_seed = opt$seed
 set.seed(mice_random_seed)
-method=mice_imputation_method
-metadata_predictors=strsplit(metadata_predictors, ",")[[1]]
-metadata_predictors <- unlist(lapply(metadata_predictors, trimws))
+method = opt$method
+additional_predictors = opt$additional_predictors
+output_path=opt$output
+imputed_only_path=opt$imputed_only
 
-cli_alert_success("Updated database.")
+if (length(additional_predictors) > 0) {
+  additional_predictors = strsplit(additional_predictors, ",")[[1]]
+  additional_predictors <- unlist(lapply(additional_predictors, trimws))
+}
+
+
+####################################################################################
+###READ INPUT FILES
+####################################################################################
 
 read_file <- function(file_path) {
   # Check file extension and read accordingly
@@ -79,23 +122,28 @@ read_file <- function(file_path) {
 }
 
 #read main dataset
-df=read_file(data_path)
+df = read_file(opt$data)
 
 #read metabolite annotation
-metabolites=read_file(chemical_annotation_path)
+metabolites = read_file(opt$chemical_annotation)
 
 
-### IDENTIFY UNNAMED AND XENOBIOTICS
+####################################################################################
+###IDENTIFY UNNAMED AND XENOBIOTIC METABOLITES
+####################################################################################
+
 assign_annotation <- function(df) {
   df %>%
-    mutate(ORIGIN = case_when(
-      is.na(SUPER_PATHWAY) ~ "UNNAMED",
-      SUPER_PATHWAY == "Xenobiotics" ~ "EXOGENOUS",
-      TRUE ~ "ENDOGENOUS"
-    ))
+    mutate(
+      ORIGIN = case_when(
+        is.na(SUPER_PATHWAY) ~ "UNNAMED",
+        SUPER_PATHWAY == "Xenobiotics" ~ "EXOGENOUS",
+        TRUE ~ "ENDOGENOUS"
+      )
+    )
 }
 
-metabolites=assign_annotation(metabolites)
+metabolites = assign_annotation(metabolites)
 
 #drop exogenous metabolites
 
@@ -105,7 +153,7 @@ metabolites <- metabolites %>%
 sample_metadata <- df %>%
   select(-one_of(metabolites$CHEM_ID))
 
-exogenous_metabolites=metabolites %>%
+exogenous_metabolites = metabolites %>%
   filter(ORIGIN == "EXOGENOUS") %>%
   pull("CHEM_ID")
 
@@ -116,49 +164,61 @@ endogenous_metabolite_ids <- metabolites %>%
 metabolites <- metabolites %>%
   filter(ORIGIN != "EXOGENOUS")
 
-metabolite_ids=c(metabolites$CHEM_ID)
+metabolite_ids = c(metabolites$CHEM_ID)
 
+#extract metabolite data only
 metabolite_data <- df %>% select(any_of(metabolite_ids))
-metabolite_data=as.data.frame(lapply(metabolite_data, as.numeric))
+metabolite_data = as.data.frame(lapply(metabolite_data, as.numeric))
 
-###COMPUTE CORRELATIONS AND REMOVE UNNAMED FROM TOP 10
-correlations <- cor(metabolite_data, method='pearson', use='pairwise.complete.obs')
+
+####################################################################################
+###COMPUTE CORRELATIONS BETWEEN METABOLITES AND REMOVE UNNAMED FROM TOP 10
+####################################################################################
+
+correlations <-
+  cor(metabolite_data, method = 'pearson', use = 'pairwise.complete.obs')
 correlations <- as_tibble(correlations)
 correlations$CHEM_ID <- colnames(correlations)
-correlations=column_to_rownames(correlations, var = "CHEM_ID")
+correlations = column_to_rownames(correlations, var = "CHEM_ID")
 correlations_endogenous_only <- correlations %>%
   rownames_to_column(var = "CHEM_ID") %>%
   filter(CHEM_ID %in% endogenous_metabolite_ids) %>%
   column_to_rownames(var = "CHEM_ID")
 
-top_10_correlated <- lapply(colnames(correlations_endogenous_only), function(col) {
-  # Extract the correlation values for the current column
-  cor_vals <- correlations_endogenous_only[[col]]
-  
-  # Remove self-correlation (correlation of the column with itself)
-  cor_vals <- cor_vals[colnames(correlations_endogenous_only) != col]
-  
-  # Get the names of the remaining columns
-  remaining_cols <- colnames(correlations_endogenous_only)[colnames(correlations_endogenous_only) != col]
-  
-  # Return the names of the columns with the top 10 correlations
-  return(remaining_cols[order(-abs(cor_vals))[1:10]])
-})
+top_10_correlated <-
+  lapply(colnames(correlations_endogenous_only), function(col) {
+    # Extract the correlation values for the current column
+    cor_vals <- correlations_endogenous_only[[col]]
+    
+    # Remove self-correlation (correlation of the column with itself)
+    cor_vals <-
+      cor_vals[colnames(correlations_endogenous_only) != col]
+    
+    # Get the names of the remaining columns
+    remaining_cols <-
+      colnames(correlations_endogenous_only)[colnames(correlations_endogenous_only) != col]
+    
+    # Return the names of the columns with the top 10 correlations
+    return(remaining_cols[order(-abs(cor_vals))[1:10]])
+  })
 
-names(top_10_correlated)=colnames(correlations_endogenous_only)
+names(top_10_correlated) = colnames(correlations_endogenous_only)
 
-### MICE IMPUTATION
+####################################################################################
+###MICE IMPUTATION
+####################################################################################
 
-### BUILD WHERE MATRIX
+#build where matrix to identify missing values
 
 where_matrix <- make.where(df)
-where_matrix[,c(colnames(sample_metadata))] <- FALSE
+where_matrix[, c(colnames(sample_metadata))] <- FALSE
 
-### BUILD PREDICTION MATRIX
+#build default prediction matrix and set all predictors to 0
 
 pred_matrix <- make.predictorMatrix(df)
 pred_matrix[, colnames(pred_matrix)] <- 0
 
+#update prediction matrix with top 10 correlated metabolites for each metabolite
 update_pred_matrix <- function(mat, predictor_list) {
   for (row_name in rownames(mat)) {
     names_to_set <- predictor_list[[row_name]]
@@ -169,21 +229,44 @@ update_pred_matrix <- function(mat, predictor_list) {
 
 pred_matrix <- update_pred_matrix(pred_matrix, top_10_correlated)
 
-if (length(metadata_predictors)>0) {
-pred_matrix[, metadata_predictors] <- 1
+#if additional predictors were specified in the arguments, set their status as 1 in the prediction matrix
+if (length(additional_predictors) > 0) {
+  pred_matrix[, additional_predictors] <- 1
 }
 
-###MICE IMPUTATION
+#impute missing values
 
-IMP <- mice(df, m=1, where = where_matrix , predictorMatrix = pred_matrix, method=mice_imputation_method, seed=mice_random_seed)
-output <- complete(IMP ,  action = 'long' , include = TRUE)#[c(".imp",metabolite)]
+IMP <-
+  mice(
+    df,
+    m = n_imputations,
+    where = where_matrix ,
+    predictorMatrix = pred_matrix,
+    method = method,
+    seed = mice_random_seed
+  )
 
-###SAVE OUTPUTS
+####################################################################################
+###SAVE OUTPUT FILES
+####################################################################################
 
-only_imputed_values <- IMP$imp
-all_imputed_values <- bind_rows(only_imputed_values, .id = "metabolite")
+#extract all imputed datasets
+output <-
+  complete(IMP ,  action = 'long' , include = TRUE)
 
-write.table(output, file=output_path, sep='\t')
-write.table(all_imputed_values, file=only_imputed_values_path, sep='\t')
+write.table(output, file = output_path, sep = '\t')
 
+#extract only imputed values (useful for testing)
+
+opt$imputed_only = NULL
+if (is.null(opt$imputed_only))
+{
+} else
+{
+  only_imputed_values <- IMP$imp
+  all_imputed_values <-
+    bind_rows(only_imputed_values, .id = "metabolite")
+  write.table(all_imputed_values, file = imputed_only_path, sep = '\t')
+  
+}
 
